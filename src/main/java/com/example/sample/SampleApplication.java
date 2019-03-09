c969334cdff217b379b505c4113e0c2cf5bbd544
Sandeep Menon <sandeep.menon@aspiresys.com>
2019-03-09 11:47:20 +0530

    Added H2 as a compile time dependency as well as created a TCP server for sharing the H2 database instance across JVM's

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -2,6 +2,10 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 
@@ -10,5 +14,10 @@ public class SampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
+    
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+    }
 
 }
